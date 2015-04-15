40b0666b5a3d25c4010d0bc8be23a513b43a605e
Franco Bouly <fbouly@gmail.com>
2015-04-15 18:53:24 +0200

    Ambiant sa mere je vais bouffer

--- taggle/src/Ambiant.js
+++ taggle/src/Ambiant.js
@@ -2,10 +2,50 @@ var EventEmitter = require('events').EventEmitter;
 var tessel = require('tessel');
 var ambientlib = require('ambient-attx4');
 
-var ambient = ambientlib.use(tessel.port['A']);
-
-var emitter;
-
-var Ambiant = function(port) {
+var Ambiant = function(port, settings) {
 	this.port = port;
+
+	this.init();
+};
+
+Ambian.prototype.init = function() {
+	this.ambiant = ambientlib.use(tessel.port[this.port]);
+
+	this.ambiant.on('ready', this._onReady.bind(this));
+	this.ambiant.on('sound-trigger', this._onSoundTrigger.bind(this));
+	this.ambiant.on('light-trigger', this._onLightTrigger.bind(this));
+	this.ambiant.on('error', this._onError.bind(this));
+}
+
+Ambiant.prototype.__proto__ = EventEmitter.prototype;
+
+Ambiant.prototype._onReady = function(){
+	// setInterval( function () {
+	//     ambient.getLightLevel( function(err, ldata) {
+	//       if (err) throw err;
+	//       ambient.getSoundLevel( function(err, sdata) {
+	//         if (err) throw err;
+	//         console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
+	//     });
+	// })}, 500);
+
+	ambient.setSoundTrigger(0.1);
+	// ambient.setLightTrigger(0.5);
+};
+
+Ambiant.prototype._onSoundTrigger = function(){
+	this.emit('shout');
+
+    this.ambient.clearSoundTrigger();
+
+    setTimeout(function () {
+        this.ambient.setSoundTrigger(0.1);
+    }.bind(this),1500);
+};
+
+Ambiant.prototype._onLightTrigger = function(){
+};
+
+Ambiant.prototype._onError = function(){
+	console.log('Error :(');
 };
