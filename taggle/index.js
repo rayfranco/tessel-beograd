4ed55a16f5239a607b5d6b39c2eff2b09f08c2f5
Franco Bouly <fbouly@gmail.com>
2015-04-17 02:21:20 +0200

    Finish Taggle

--- taggle/index.js
+++ taggle/index.js
@@ -1,4 +1,9 @@
 var Ambiant = require('./src/Ambiant');
 
-var ambiant = Ambiant('A');
+var ambiant = new Ambiant('A');
+
+ambiant.on('shout',function(){
+  console.log('TAGGLE');
+})
+
 // var sound = Sound('B');
--- taggle/src/Ambiant.js
+++ taggle/src/Ambiant.js
@@ -4,18 +4,21 @@ var ambientlib = require('ambient-attx4');
 
 var Ambiant = function(port, settings) {
 	this.port = port;
+	this.ambiant = ambientlib.use(tessel.port[this.port]);
 
 	this.init();
 };
 
-Ambian.prototype.init = function() {
-	this.ambiant = ambientlib.use(tessel.port[this.port]);
+Ambiant.prototype.proxy = function(fn, ctx) {
+	return fn.call(ctx);
+};
 
+Ambiant.prototype.init = function() {
 	this.ambiant.on('ready', this._onReady.bind(this));
 	this.ambiant.on('sound-trigger', this._onSoundTrigger.bind(this));
 	this.ambiant.on('light-trigger', this._onLightTrigger.bind(this));
 	this.ambiant.on('error', this._onError.bind(this));
-}
+};
 
 Ambiant.prototype.__proto__ = EventEmitter.prototype;
 
@@ -28,18 +31,19 @@ Ambiant.prototype._onReady = function(){
 	//         console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
 	//     });
 	// })}, 500);
+	// 
 
-	ambient.setSoundTrigger(0.1);
+	this.ambiant.setSoundTrigger(0.1);
 	// ambient.setLightTrigger(0.5);
 };
 
 Ambiant.prototype._onSoundTrigger = function(){
 	this.emit('shout');
 
-    this.ambient.clearSoundTrigger();
+    this.ambiant.clearSoundTrigger();
 
     setTimeout(function () {
-        this.ambient.setSoundTrigger(0.1);
+        this.ambiant.setSoundTrigger(0.1);
     }.bind(this),1500);
 };
 
@@ -49,3 +53,5 @@ Ambiant.prototype._onLightTrigger = function(){
 Ambiant.prototype._onError = function(){
 	console.log('Error :(');
 };
+
+module.exports = Ambiant;
