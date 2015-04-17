477a1d11e94a0e48b5f2092e1354e773656b6045
Franco Bouly <fbouly@gmail.com>
2015-04-17 20:17:25 +0200

    Remove debug

--- taggle/index.js
+++ taggle/index.js
@@ -1,13 +1,9 @@
 var tessel = require('tessel');
 
-// var Ambiant = require('./src/Ambiant');
-// var Sound = require('./src/Sound');
+var Ambiant = require('./src/Ambiant');
+var Sound = require('./src/Sound');
 
 var ambientlib = require('ambient-attx4');
-console.log(ambientlib.use);
-// ambientlib.updateFirmware( tessel.port['A'], 'node_modules/ambient-attx4/firmware/src/ambient-attx4.hex');
-
-return;
 
 var ambiant = new Ambiant('A',{
   limit: 0.1
@@ -18,15 +14,15 @@ ambiant.on('shout',function(){
   sound.taggle();
 
   // Blink led
-  var timeout;
-  (function blink(value) {
-    tessel.led[value ? 0 : 1].write(value);
-    console.log('set to ', value);
-    timeout = setTimeout(blink, 200, !value);
-  })(true);
-  setTimeout(function(){
-    clearTimeout(timeout);
-  },1000);
+  // var timeout;
+  // (function blink(value) {
+  //   tessel.led[value ? 0 : 1].write(value);
+  //   console.log('set to ', value);
+  //   timeout = setTimeout(blink, 200, !value);
+  // })(true);
+  // setTimeout(function(){
+  //   clearTimeout(timeout);
+  // },1000);
 });
 
 // var sound = Sound('B');
